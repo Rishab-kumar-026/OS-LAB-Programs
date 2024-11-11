450c5c12af6c49ec02e115ac6c0297ef52215b23
Rishab-kumar-026 <rks2034287@gmail.com>
2024-11-11 20:12:25 +0530

    OS Programs

--- First_Come_First_Scheduling.c
+++ First_Come_First_Scheduling.c
@@ -1,50 +1,59 @@
- #include <stdio.h>
- void findWaitingTime(int processes[], int n, int burstTime[], int waitingTime[]) {
- // Waiting time for first process is 0
- waitingTime[0] = 0;
- // Calculating waiting time for each process
- for (int i = 1; i < n; i++) {
- waitingTime[i] = burstTime[i- 1] + waitingTime[i- 1];
- }
- }
- void findTurnaroundTime(int processes[], int n, int burstTime[], int waitingTime[], int
- turnaroundTime[]) {
- // Calculating turnaround time for each process
- for (int i = 0; i < n; i++) {
- turnaroundTime[i] = burstTime[i] + waitingTime[i];
- }
- }
- void findAverageTime(int processes[], int n, int burstTime[]) {
- int waitingTime[n], turnaroundTime[n], totalWaitingTime = 0, totalTurnaroundTime = 0;
- // Calculate waiting time for all processes
- findWaitingTime(processes, n, burstTime, waitingTime);
- // Calculate turnaround time for all processes
- findTurnaroundTime(processes, n, burstTime, waitingTime, turnaroundTime);
- // Display process details
- printf("Processes\tBurst Time\tWaiting Time\tTurnaround Time\n");
- for (int i = 0; i < n; i++) {
- totalWaitingTime += waitingTime[i];
- totalTurnaroundTime += turnaroundTime[i];
- printf("%d\t\t%d\t\t%d\t\t%d\n", processes[i], burstTime[i], waitingTime[i],
- turnaroundTime[i]);
- }
- // Calculate and display average waiting time and average turnaround time
- printf("\nAverage Waiting Time: %.2f", (float)totalWaitingTime / n);
- printf("\nAverage Turnaround Time: %.2f\n", (float)totalTurnaroundTime / n);
+#include <stdio.h>
+int i;
+int findWaitingTime(int processes[], int n, int burstTime[], int waitingTime[])
+{
+    waitingTime[0] = 0;
+    for (i = 1; i < n; i++)
+    {
+        waitingTime[i] = waitingTime[i - 1] + burstTime[i - 1];
+    }
 }
- int main() {
- int n;
- // Input the number of processes
- printf("Enter the number of processes: ");
- scanf("%d", &n);
- int processes[n], burstTime[n];
- // Input burst time for each process
- for (int i = 0; i < n; i++) {
- processes[i] = i + 1;
- printf("Enter burst time for process %d: ", i + 1);
- scanf("%d", &burstTime[i]);
- }
- // Calculate average time using FCFS scheduling
- findAverageTime(processes, n, burstTime);
- return 0;
- }
+
+int findTurnAroundTime(int processes[], int n, int burstTime[], int waitingTime[], int turnAroundTime[])
+{
+    for (i = 0; i < n; i++)
+    {
+        turnAroundTime[i] = waitingTime[i] + burstTime[i];
+    }
+}
+
+int findAverageTime(int processes[], int n, int burstTime[])
+{
+    int waitingTime[n], turnAroundTime[n], totalWaitingTime = 0, totalTurnAroundTime = 0;
+
+    findWaitingTime(processes, n, burstTime, waitingTime);
+    findTurnAroundTime(processes, n, burstTime, waitingTime, turnAroundTime);
+
+    printf("Processes \t WaitingTime \t BurstTime \t TurnAroundTime");
+    printf("\n");
+    for (i = 0; i < n; i++)
+    {
+        totalWaitingTime = totalWaitingTime + waitingTime[i];
+        totalTurnAroundTime = totalTurnAroundTime + turnAroundTime[i];
+
+        printf("%d \t %d \t %d \t %d", processes[i], waitingTime[i], burstTime[i], turnAroundTime[i]);
+        printf("\n");
+    }
+    printf("\n");
+    printf("Average Waiting Time: %.2f", (float)totalWaitingTime / n);
+    printf("\n Average TurnAround Time: %.2f", (float)totalTurnAroundTime / n);
+}
+
+int main()
+{
+    int n;
+    printf("Enter the number of Process: ");
+    scanf("%d", &n);
+
+    int processes[n], burstTime[n];
+
+    for (i = 0; i < n; i++)
+    {
+        processes[i] = i + 1;
+        printf("Enter the Burst Time for Process %d : ", i + 1);
+        scanf("%d", &burstTime[i]);
+    }
+
+    findAverageTime(processes, n, burstTime);
+    return 0;
+}
--- IO_SystemCalls.c
+++ IO_SystemCalls.c
@@ -1,41 +1,43 @@
-#include <stdio.h>
- #include <stdlib.h>
- #include <fcntl.h>
- #include <unistd.h>
- #define BUFFER_SIZE 1024
- int main() {
- int sourceFile, destFile;
- ssize_t bytesRead, bytesWritten;
- char buffer[BUFFER_SIZE];
-// Open the source file for reading
- sourceFile = open("source.txt", O_RDONLY);
- if (sourceFile < 0) {
- perror("Error opening source file");
- exit(1);
- }
- // Open the destination file for writing (create if it doesn't exist)
- destFile = open("destination.txt", O_WRONLY | O_CREAT | O_TRUNC, 0644);
- if (destFile < 0) {
- perror("Error opening destination file");
- close(sourceFile);
- exit(1);
- }
- // Read from the source file and write to the destination file
- while ((bytesRead = read(sourceFile, buffer, BUFFER_SIZE)) > 0) {
- bytesWritten = write(destFile, buffer, bytesRead);
- if (bytesWritten != bytesRead) {
- perror("Error writing to destination file");
- close(sourceFile);
- close(destFile);
- exit(1);
- }
- }
- if (bytesRead < 0) {
- perror("Error reading from source file");
- }
- // Close both files
- close(sourceFile);
- close(destFile);
- printf("File copied successfully.\n");
- return 0;
- }
+#include<stdio.h>
+#include<stdlib.h>//used for functions like exit()
+#include<fcntl.h>
+#include<unistd.h>//both are used for read,write,close,open files
+#define BUFFER_SIZE 1024// determines the number of bytes read from the source file at a time.
+int main(){
+    int sourceFile,destinationFile;
+    ssize_t bytesRead ,bytesWritten;//determines how much data is read and written in/from source&destination files
+    char buffer[BUFFER_SIZE];
+    
+    sourceFile = open("source.txt",O_RDONLY);
+    if(sourceFile<0){
+        perror("Error while opening source File");
+        exit(1);
+    }
+    
+    destinationFile = open("destination.txt",O_WRONLY|O_CREAT|O_TRUNC,0644);
+    //0644 making the file readable and writable by the owner, and readable by others.
+    
+    if(destinationFile<0){
+        perror("Error while opening Destination File");
+        close(sourceFile);
+        exit(1);
+    }
+    
+    while(bytesRead = read(sourceFile,buffer,BUFFER_SIZE)){
+        bytesWritten = write(destinationFile,buffer,bytesRead);
+    if(bytesRead != bytesWritten){
+        perror("Doesn't copied successfully");
+    }
+    close(sourceFile);
+    close(destinationFile);
+    exit(1);
+    }
+
+if(bytesRead < 0){ //checks whether source file is empty or not
+    perror("Error reading from source File");
+}
+close(sourceFile);
+close(destinationFile);
+
+printf("Successfully Copied from Source File and Written in Destination File");
+}
